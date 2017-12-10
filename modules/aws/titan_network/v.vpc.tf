ae8487534e71bc4f5b3972c93732c9c8f1243140
Naftuli Kay <me@naftuli.wtf>
2017-12-10 13:14:32 -0800

    Route 53 hackery.

---
+++ modules/aws/titan_network/v.vpc.tf
@@ -0,0 +1,11 @@
+# TITAN Network Module - VPC Variables
+
+variable "network_id" {
+  description = <<-EOF
+    The second octet to use for this TITAN network.
+
+    In compliance with RFC 1918, the first octet is hard-coded to 10, as this is the only reserved address block which
+    is a `/8` dedicated to private networking. This `network_id` is the second octet and is used to create a `/16` for
+    this TITAN network.
+  EOF
+}
